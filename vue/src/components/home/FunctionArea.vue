75a4661577b72fd68188afed1a43acdff1b617ae
mogiven <2359353909@qq.com>
2022-12-04 17:21:20 +0800

    完成主页布局
    更新前端一部分

---
+++ vue/src/components/home/FunctionArea.vue
@@ -0,0 +1,32 @@
+<template>
+    <el-card class="box-card2">
+      <template #header>
+        <div class="card-header">
+          <span>Card name</span>
+          <el-button class="button" text>Operation button</el-button>
+        </div>
+      </template>
+      <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+    </el-card>
+  </template>
+  
+  <style>
+  .card-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  .text {
+    font-size: 14px;
+  }
+  
+  .item {
+    margin-bottom: 18px;
+  }
+  
+  .box-card2 {
+    width: 250px;
+  }
+  </style>
+  
--- vue/src/view/Myspace.vue
+++ vue/src/view/Myspace.vue
@@ -1,47 +1,61 @@
 <template>
-  <el-radio-group v-model="isCollapse" >
-    <el-radio-button :label="!isCollapse">
-      <el-icon ><Grid /></el-icon>
-    </el-radio-button>
-  </el-radio-group>
-  <el-menu
-    default-active="2"
-    class="el-menu-vertical-demo"
-    :collapse="isCollapse"
-    @open="handleOpen"
-    @close="handleClose"
-  >
-
-    <router-link class="alink" to="../myspace/userInfo">
-      <el-menu-item index="1">
-        <el-icon><User /></el-icon>
-        <template #title>
-          我的账户信息
-        </template>
-      </el-menu-item>
-    </router-link>
-
-    <router-link class="alink" to="../myspace/myProfile">
-      <el-menu-item index="2">
-        <el-icon><icon-menu /></el-icon>
-        <template #title>我的简历</template>
-      </el-menu-item>
-    </router-link>
-    
-    <router-link class="alink" to="../myspace/historyPostList">
-      <el-menu-item index="3">
-        <el-icon><document /></el-icon>
-        <template #title>历史投递列表</template>
-      </el-menu-item>
-    </router-link>
-    
-    <router-link class="alink" to="../myspace/sysNotice">
-      <el-menu-item index="4">
-        <el-icon><setting /></el-icon>
-        <template #title>通知</template>
-      </el-menu-item>
-    </router-link>
-  </el-menu>
+  <div class="myspace">
+    <el-container class="myspace">
+      <el-aside >
+        <el-radio-group v-model="isCollapse" >
+          <el-radio-button :label="!isCollapse">
+            <el-icon ><Grid /></el-icon>
+          </el-radio-button>
+        </el-radio-group>
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          :collapse="isCollapse"
+          @open="handleOpen"
+          @close="handleClose"
+        >
+      
+          <router-link class="alink" to="../myspace/userInfo">
+            <el-menu-item index="1">
+              <el-icon><User /></el-icon>
+              <template #title>
+                我的账户信息
+              </template>
+            </el-menu-item>
+          </router-link>
+      
+          <router-link class="alink" to="../myspace/myProfile">
+            <el-menu-item index="2">
+              <el-icon><icon-menu /></el-icon>
+              <template #title>我的简历</template>
+            </el-menu-item>
+          </router-link>
+          
+          <router-link class="alink" to="../myspace/historyPostList">
+            <el-menu-item index="3">
+              <el-icon><document /></el-icon>
+              <template #title>历史投递列表</template>
+            </el-menu-item>
+          </router-link>
+          
+          <router-link class="alink" to="../myspace/sysNotice">
+            <el-menu-item index="4">
+              <el-icon><setting /></el-icon>
+              <template #title>通知</template>
+            </el-menu-item>
+          </router-link>
+        </el-menu>
+      </el-aside>
+  
+      <el-main class="pro">
+        <div style="width: 80% ; float:left">
+          <router-view></router-view>
+        </div>
+      </el-main>
+    </el-container>
+  </div>
+  
+  
 </template>
 
 <script lang="ts" setup>
@@ -65,10 +79,19 @@ const handleClose = (key: string, keyPath: string[]) => {
 
 <style>
 .el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
-  min-height: 400px;
+  width: 150px;
+  min-height: 600px;
 }
 .alink{
   text-decoration: none;
+}
+
+
+.myspace{
+  background-color: rgb(245, 245, 245);
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
 }
 </style>
